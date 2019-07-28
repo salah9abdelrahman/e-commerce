d57c33dca1142616589764eee53e3b48e503d98e
salah rahim <salah9abdelrahman@outlook.com>
2019-07-28 10:49:42 +0200

    Add project files.

---
+++ commerce/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="commerce.MvcApplication" Language="C#" %>
---
+++ commerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(commerce.Startup))]
+namespace commerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
