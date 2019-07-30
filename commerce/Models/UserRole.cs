d2d4656bf355ba4d00a0ee0a6b8d80182260c35c
salah rahim <salah9abdelrahman@outlook.com>
2019-07-30 14:06:52 +0200

    fixing PK of UserRole

--- commerce/Models/UserRole.cs
+++ commerce/Models/UserRole.cs
@@ -7,6 +7,7 @@ namespace commerce.Models
 {
     public class UserRole : RowInformation
     {
+        public int UserRoleId { get; set; }
         public int UserId { get; set; }
         public int RoleId { get; set; }
 
