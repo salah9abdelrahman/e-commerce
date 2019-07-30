879dc030884393db3dd02630ec281848ae663188
salah rahim <salah9abdelrahman@outlook.com>
2019-07-30 13:58:40 +0200

    database
    EF code first

---
+++ commerce/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace commerce.Models
+{
+    public class Role : RowInformation
+    {
+        public int RoleId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
