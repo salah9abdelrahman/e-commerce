b0d24b9f0950257a64bc7a7e6ccfa2db8c174b93
salah rahim <salah9abdelrahman@outlook.com>
2019-07-31 10:27:17 +0200

    fixing references  in views

---
+++ commerce/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commerce.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        //to use lamda expression to filter objects exactly like where method
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ commerce/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace commerce.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        //we declare it protected to used in the repositories
+        protected readonly DbContext dbContext;
+        public Repository(DbContext context)
+        {
+            dbContext = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            dbContext.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            dbContext.Set<TEntity>().AddRange(entities);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbContext.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity Get(int id)
+        {
+            return dbContext.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return dbContext.Set<TEntity>().ToList();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            dbContext.Set<TEntity>().Remove(entity);
+
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            dbContext.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
