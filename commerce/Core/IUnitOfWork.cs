64276f4c3184aa377e35eaac6ebeb119ce121d81
salah rahim <salah9abdelrahman@outlook.com>
2019-07-31 11:41:10 +0200

    fixing the structure and add unit of work

---
+++ commerce/Core/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace commerce.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //here we put the specific repositories interfaces to mock unit test
+        // and complete (or save) function
+        int Complete();
+    }
+}
---
+++ commerce/Repositories/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace commerce.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _dbContext = context;
+            //use dbsets to initialize repositories
+        }
+
+        public int Complete()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
